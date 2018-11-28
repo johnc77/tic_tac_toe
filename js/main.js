9ab93fbfeeed90f2af865e2092bb2887b761e054
johnc77 <jcunningham77@hotmail.co.uk>
2018-11-28 15:32:22 +0800

    Made a start on the main logic for the game. Added some links to elements, player name functions and function to check if the computer is a player or not before assigning a name.

--- js/main.js
+++ js/main.js
@@ -10,3 +10,40 @@ function closeSlideMenu(){
     document.getElementById('main').style.marginLeft = '0px';
 }
 
+/* FLAGS */
+
+let computerPlayer = false;
+
+/* ELEMENTS */
+
+const compPlayerBtn = document.querySelector("#p2comp");
+const playerOneDisplay = document.getElementById('p1Dname');
+const playerNameOne = document.getElementById('p1name');
+const playerTwoDisplay = document.getElementById('p2Dname');
+const playerNameTwo = document.getElementById('p2name');
+const soundEffects = document.querySelector('#sounds');
+
+
+/* NAME INPUTS & MENU BUTTONS */
+
+function playerOneName(){
+    playerOneDisplay.innerHTML = "PLAYER ONE: " + playerNameOne.value;
+}
+
+function playerTwoName(){
+    if(computerPlayer == true) {
+        alert("Player Two is the computer. Please switch off computer player for two player mode.")
+    } else {
+    playerTwoDisplay.innerHTML = "PLAYER TWO: " + playerNameTwo.value;
+    }
+};
+
+compPlayerBtn.addEventListener('click', (event) => {
+    if(compPlayerBtn.checked == true) {
+        computerPlayer = true;
+    playerTwoDisplay.innerHTML = "PLAYER TWO: COMPUTER";
+    } else {
+    computerPlayer = false;
+    playerTwoDisplay.innerHTML = "PLAYER TWO";
+    }
+});
