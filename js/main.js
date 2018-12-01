4464545f002620dd613d55c91046ed57a3a5ecab
johnc77 <jcunningham77@hotmail.co.uk>
2018-12-01 16:51:04 +0800

    Added function to start game and new turn. Need to consider how to get the display to flow from one message to another in a given time. Need to consider how to activate and de-activate game cells, then make a place counter function.

--- js/main.js
+++ js/main.js
@@ -10,10 +10,6 @@ function closeSlideMenu(){
     document.getElementById('main').style.marginLeft = '0px';
 }
 
-/* FLAGS */
-
-let computerPlayer = false;
-
 /* ELEMENTS */
 
 const compPlayerBtn = document.querySelector("#p2comp");
@@ -22,11 +18,44 @@ const playerNameOne = document.getElementById('p1name');
 const playerTwoDisplay = document.getElementById('p2Dname');
 const playerNameTwo = document.getElementById('p2name');
 const soundEffects = document.querySelector('#sounds');
+const messageBoard = document.getElementById('messageBoard');
+const startGameBtn = document.getElementById('start');
+
+/* FLAGS */
+
+let playerOne = {
+    id: "playerOne",
+    name: ""
+};
+let playerTwo = {
+    id: "playerTwo",
+    name: ""
+};
+
+let computerPlayer = false;
+let currentPlayer = "";
+let playerOneMoves = [];
+let playerTwoMoves = [];
+let freeSquares = [1,2,3,4,5,6,7,8,9];
+const winningCombo = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [1,4,7],
+    [2,5,8],
+    [3,6,9],
+    [1,5,9],
+    [3,5,7]
+];
+
+
 
 
 /* NAME INPUTS & MENU BUTTONS */
 
+//Sets player names and prints to display
 function playerOneName(){
+    playerOne.name = playerNameOne.value;
     playerOneDisplay.innerHTML = "PLAYER ONE: " + playerNameOne.value;
 }
 
@@ -34,16 +63,64 @@ function playerTwoName(){
     if(computerPlayer == true) {
         alert("Player Two is the computer. Please switch off computer player for two player mode.")
     } else {
+    playerTwo.name = playerNameTwo.value;
     playerTwoDisplay.innerHTML = "PLAYER TWO: " + playerNameTwo.value;
     }
 };
 
+//Sets flag for computer player and prints to display
 compPlayerBtn.addEventListener('click', (event) => {
     if(compPlayerBtn.checked == true) {
         computerPlayer = true;
+        
     playerTwoDisplay.innerHTML = "PLAYER TWO: COMPUTER";
     } else {
     computerPlayer = false;
     playerTwoDisplay.innerHTML = "PLAYER TWO";
+    
     }
 });
+
+//sets listener for start game button
+
+startGameBtn.addEventListener ('click', (event) => {
+    newGame();
+    newTurn();
+})
+/* GAME FUNCTIONS */
+
+function updateDisplay(player){
+    messageBoard.innerHTML = player.name + " READY?";
+    setTimeout(updateDisplayTurn(currentPlayer), 2000);
+}
+
+function updateDisplayTurn(player){
+    if(player == playerOne){
+    messageBoard.innerHTML = player.name + " PLACE YOUR X";
+    } else {
+    messageBoard.innerHTML = player.name + " PLACE YOUR O";
+    }
+}
+
+function updateCurrentPlayer(){
+    if(currentPlayer == "") {
+        currentPlayer = playerOne;
+    } else if(currentPlayer == "playerOne"){
+        currentPlayer = playerTwo;
+    } else {
+        currentPlayer = playerOne;
+    }
+}
+
+function newGame(){
+    playerOneMoves = [];
+    playerTwoMoves = [];
+    freeSquares = [1,2,3,4,5,6,7,8,9]; 
+}
+
+function newTurn(){
+    updateCurrentPlayer();
+    console.log(currentPlayer);
+    console.log(playerOne.name);  
+    updateDisplay(currentPlayer);
+}
